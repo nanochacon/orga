12f90a7b17acfb9667e1a5af2dd8dae12c23346f
Nicolas Soto <sotop.nicolas@gmail.com>
2014-10-05 15:04:05 -0400

    Primera version

--- Ln0.c
+++ Ln0.c
@@ -56,7 +56,7 @@ int main(int argc, char *argv[]){
 	  
 	  double total; 
 	  for (a=0; a < i; a++){ //todas las iteraciones (i=numero de iteraciones ingresadas)
-	    total = ln(num); 
+	      total = ln(num); 
 	  }
 	printf("%f\n", total); 
 	}
