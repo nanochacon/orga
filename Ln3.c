4eba80b83890535a24ffafc68b5cda9006c006e6
Nicolas Soto <sotop.nicolas@gmail.com>
2014-10-05 14:57:10 -0400

    mensaje

--- Ln3.c
+++ Ln3.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+
 double ln(double n)
-{ 	double resultado,x,y,z,c1,c2,c3,c4,c5,c6;
+{	double resultado,x,y,z,c1,c2,c3,c4,c5,c6;
 	y=(n-1);
 	z=(n+1);
 	x=(y/z);
@@ -17,17 +21,53 @@ double ln(double n)
 	+ x*x*x*x*x*x*x*x*x*c4 
 	+ x*x*x*x*x*x*x*x*x*x*x*c5 
 	+ x*x*x*x*x*x*x*x*x*x*x*x*x*c6;
- 	return 2*resultado;
- } 
-int main(){ 
-	double numero; 
-	int i,iteraciones;
-	printf("ingrese un numero: "); 
-	scanf("%lf",&numero); 
-	printf("ingrese cantidad de iteraciones: "); 
-	scanf("%i",&iteraciones); 
-	double total; 
-	for (i=0; i < iteraciones; i++){ 
-	total = ln(numero); 
-	} 
-	printf(" %f\n", total); }
+	return 2*resultado;
+} 
+
+int main(int argc, char *argv[]){
+	int i;// para guardar el numero ingresado
+	double num; //para guardar la cantidad de iteraciones
+	int a; //para el ciclo for
+	int b; //para leer usando getopt
+	int errflg=0; //para gestionar los ingresos incorrectos
+	
+	char *valorIteracion; //cantidad de iteraciones ingresadas
+	char *valorNumero; //numero ingresado
+	
+	extern int optopt;
+	extern char *optarg;
+	
+	while ((b = getopt (argc, argv, "i:n:")) != -1){
+		switch (b){
+			case 'i':
+				valorIteracion = optarg; //guarda el contenido de lo que sigue a i (argumento)
+				i=atoi(valorIteracion); //transforma el valor a entero y lo guarda en i
+				break;
+			case 'n':
+				valorNumero = optarg; //guarda el contenido de lo que sigue a n (argumento) en el comando
+				num=atoi(valorNumero); //transforma el valor a entero y lo guarda en n
+				break;
+			case ':':        //si ingresa cualquier otra cosa 
+				fprintf(stderr,"Option -%c requires an operand\n", optopt);
+				errflg++;
+				break;
+			case '?':
+				fprintf(stderr,"Unrecognized option: -%c\n", optopt);
+				errflg++;
+				break;
+		}
+	}
+	if (errflg) {
+		fprintf(stderr, "usage: . . . ");
+		exit(2);
+	}
+	else{
+	  
+	  double total; 
+	  for (a=0; a < i; a++){ //todas las iteraciones (i=numero de iteraciones ingresadas)
+	    total = ln(num); 
+	  }
+	printf("%f\n", total); 
+	}
+} 
+
